b1a5711e080e3d93cbf8215a948f6f1d04b8b13e
Sethukumar7788 <ssethukumar2004@gmail.com>
2025-08-07 12:13:19 +0530

    Add files via upload

---
+++ index.s
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nested List</title>
+    <link rel="stylesheet" href="str.css">
+</head>
+<body>
+    <h1>Nested List</h1>
+    <ol type="1">
+        <li><b><mark>History</mark></b>
+            
+            <ol type="i"> <img src="sethu.jpg">
+                <li>Background</li><p>The Indian cricket team has a rich history,
+                     evolving from its introduction by European sailors in the 18th
+                      century to becoming a global powerhouse. Initially, the team
+                       struggled, taking nearly 20 years to secure its first Test victory.</p>
+                <li>Foundation</li><p>The foundation of a cricket team involves
+                     several key aspects: establishing <b>a governing body,
+                        defining player roles and responsibilities, and fostering
+                         a positive team environment</b>. The governing body, like the
+                        BCCI in India, sets rules, organizes matches, and manages 
+                        the overall structure of the sport.</p>
+                <li>Expansion and termination</li>
+              <br> 
+            </ol>
+        </li>
+        <hr>
+        <li><b><mark>Administration</mark></b>
+            <ol type="i"> <img src="sethu2.jpg">
+<li><p>Cricket administration refers to the management and 
+    governance of the sport of cricket at various levels</li>
+   <li> <p >This includes the organizations
+    that oversee the sport, develop rules, and organize competitions.</p></li>
+    <li>The most prominent body is the International Cricket Council (ICC),
+        which governs the sport globally.<p></li>  
+            <li> At the national level, bodies
+                like the Board of Control for Cricket</p></li></li></ol>
+        <br> 
+     <hr>
+
+            <li><b><mark>Organisation</mark></b>
+                     <ol type="i"> <img src="sethu3.jpg">
+                        <li>Tournament format</li>
+                        <p>Tournament structures dictate how competitors 
+                            are matched and progress through a competition
+                            . Common formats include single elimination, double elimination, and round robin</p>
+                        <li> Player acquisition, squad composition and salaries</li>
+                        <p>In professional sports, player acquisition, squad composition,
+                             and salaries are crucial aspects of team management.
+                              Player acquisition involves identifying and recruiting 
+                              new players through various methods like auctions, trades, or free agency. </p>
+                        <li> Match rules</li> 
+                        <p>Match rules are sets of criteria used to 
+                            determine if different records represent the same entity</p>
+                         <li>Prize money</li>
+                        <p>Prize money refers to the
+                             financial rewards offered to winners 
+                             of competitions or individuals recognized 
+                             for exceptional achievements. </p></ol>
+                        
+                         <hr>
+             <li><b><mark>Teams</mark></b> 
+                <ol type="i"><img src="sethu4.jpg">
+                    <li>Current teams</li>
+                    <p>The India men's national cricket team is currently 
+                        playing under the captaincy of Shubman Gill in Tests, 
+                        Rohit Sharma in ODIs, and Suryakumar Yadav in T20Is</p>
+                    <li>Defunct teams</li><p>Defunct teams are sports teams<i>that
+                         no longer exist. This can be due to various reasons,
+                        including relocation,</i>
+                          merger, or simply ceasing operations.</p>
+                   <li>Timeline</li>
+                <p><amp>Cricket, a bat-and-ball game, has a long history
+                    , with its earliest records dating back to the late</amp>
+                     16th century in England. It developed into an 
+                     established sport in the 18th century</p> </ol>
+                </li>
+                
+                <hr>
+                <li><b><mark>Tournament seasons and results</mark></b></li>
+                <ol type="i"> <img src="sethu6.jpg">
+                    <li>Performance in the IPL by title</li>
+                    <p>The most successful teams in the
+                         Indian Premier League (IPL) are Mumbai 
+                         Indians and Chennai Super Kings, each with 5 titles</p>
+                      <li>IPL season results</li>
+                      <p>The Indian Premier League (IPL) is a domestic,
+                         annual Twenty20 cricket tournament played in
+                          India since 2008.[3][4] It is organized by
+                           the IPL Governing Council</p>
+                </ol>
+                <hr>
+
+                <li><b><mark>Teams performance</mark></b>
+                    <ol type="i"><img src="sethu8.jpg">
+                        <li>League stage positions</li> 
+                        <p>In a league stage, teams are ranked based on their performance, with the top positions often reserved for teams with the best records and the bottom positions for those with the worst</p>
+                    </ol>
+                   </li>
+                   <hr>
+               
+                <li><b><mark>Awards</mark></b>
+                   
+                    <ol type="i"><img src="sethu5.jpg">
+                        <li>Orange Cap</li>
+                        <p>The Orange Cap, currently known as the Aramco Orange Cap for sponsorship reasons,[1] is presented to the leading run scorer in the Indian Premier League (IPL).[2][3] It was introduced on 25 April 2008, a week after the start of the inaugural season of the IPL</p>
+                       <li>Purple Cap</li>
+                       <p>The Purple Cap, currently known as the Aramco Purple Cap for sponsorship reasons,[1] is presented to the leading wicket-taker in the IPL.[2][22] After the introduction of Orange Cap on 25 April 2008, the IPL announced the introduction of the Purple Cap on 13 May 2008.</p> 
+                       <li>Most Valuable Player</li> 
+                       <p>In the recent Indian Premier League (IPL) season, Suryakumar Yadav was named the Most Valuable Player (MVP), while Sai Sudharsan received the Emerging Player of the Year award</p>
+                       <li> Faimlay Award</li>
+                       <p>It appears there isn't a specific award widely known as the "Faimlay Award." However, several organizations and awards focus on recognizing achievements related to families, family businesses, or individuals within a family context. </p>
+                      <li> Emerging player award</li> 
+                      <p>The "Emerging Player Award" in the Indian Premier League (IPL) is given to the best young player who has shown exceptional talent and potential during the season. In IPL 2025, Sai Sudharsan of the Gujarat Titans won the award. </p>
+                        <li>Most scores Award</li</li>
+                        <p>The first awarding ceremony was held on 7 September 2004 in London, England. Between 2009 and 2014 the awards were known as the LG ICC Awards for sponsorship reasons.[2]</p>
+                  </ol>
+                  <hr>
+      <li><b><mark>Financials</mark></b>
+    <ol type="i"> <img src="sethu7.jpg">
+        <li> Title Sponsorship</li> 
+        <p>Title sponsorships, like the one with Tata for the IPL, bring in 
+            substantial revenue, which can be reinvested to improve the league, 
+            attract better players, and enhance the overall fan experience.</p>
+        <li>Payments to foreign national boards<p>A well-known title sponsor can expose the league to a wider audience, including those who may not be traditional followers of the sport. This increased visibility helps elevate the league's brand on a global scale.</p></li>
+       <li> Brand value</li> 
+    <p>Brand value represents the financial worth of a
+         brand, reflecting its overall impact on a business,
+          including its ability to generate revenue</p> </ol>
+      
+                    </li><hr>
+                    <li><b><mark>Broadcasting</mark></b>
+                        <ol type="i"><img src="sethu9.jpg">
+                            <li>Sony and WSG (2008-2017)</li> 
+                            <p>The Indian Premier League (IPL) broadcasting rights 
+                                were held by Sony and WSG (2008-2017), then by Star
+                                 India (2018-2022), and will be with international broadcasters</p>
+                            <li>Star India(2018-2022)</li>
+                            <p>Star India held the Indian Premier League (IPL)
+                                 media rights, both television and digital, for the 2018-2022 cycle</p>
+                            <li> 2023-2027</li>
+                            <p>From 2018 to 2022, Star India held the 
+                                Indian Premier League (IPL) media rights for both television and digital platforms.
+                                 They secured these rights with a consolidated global bid of Rs 16,347.50 crore</p>
+                            <li>International broadcasters</li>
+                        <p>Examples of international broadcasters
+                             include networks such as the United States' Voice of America (VOA) 
+                             and Radio Free Europe/Radio Liberty</p></ol>
+                        </li><hr>
+                        <li><b><mark>Controversy</mark></b>
+                            <ol type="i"><img src="sethu10.jpg">
+                                <li>2012 and 2013 IPL spot facing and betting match</li> 
+                                <p>The 2012 and 2013 seasons of the Indian Premier League (IPL)
+                                     were marred by spot-fixing and betting scandals, leading to
+                                      significant repercussions for the league and its sponsors</p>
+                                <li> Sponsorship</li>
+                            <p>company or organization that provides financial or
+                                 in-kind support to sporting event, often a specific
+                                  match, in exchange for promotional benefits</p></ol>
+                           </li>
+                           <hr>
+<li><d>See also</d></li>
+<p>https://en.wikipedia.org/wiki/Cricket_in_India#:~:text=Cricket%20is%20the%20most%20popular,and%20women's%20national%20cricket%20team.</p>
+<hr>
+<li><d>References</d></li><p>\wiki\Glossary_of_cricket_terms</p>
+<hr>
+<li><d>External links</d></li><p>\shop\cricket\cricket_books\cricket_reference</p>
+<hr>
+</body>
+</html>
